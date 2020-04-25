905e3e08d4c28f7f5973f2835625d89c68b4613f
alexey zotov <zotov.alexei.1@yandex.ru>
2020-04-25 11:05:50 +0400

    Step 1. Opening an image

---
+++ main.py
@@ -0,0 +1,13 @@
+from PIL import Image, ImageDraw, ImageFont
+
+
+def make_meme(image, top_text="", bottom_text=""):
+    pass
+
+
+def main():
+    image = Image.open("templates/boromir.jpg")
+    image.show()
+
+
+main()

bc1962de4de7a49c4337ff271bf871bea83cf517
alexey zotov <zotov.alexei.1@yandex.ru>
2020-04-25 11:20:30 +0400

    Step 2. Writing text on an image

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ from PIL import Image, ImageDraw, ImageFont
 
 
 def make_meme(image, top_text="", bottom_text=""):
-    pass
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype("impact.ttf", 50)
+    white_color = (255, 255, 255)
+    draw.text((10, 10), top_text, white_color, font)
+    return image
 
 
 def main():
     image = Image.open("templates/boromir.jpg")
+    image = make_meme(image, "Sample Text")
+    image.save("meme_result.jpg")
     image.show()
 
 

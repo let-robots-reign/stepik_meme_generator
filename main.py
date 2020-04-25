c932d23d1d7d93aa5834de27cbb1148daaa3dcff
alexey zotov <zotov.alexei.1@yandex.ru>
2020-04-25 15:50:46 +0400

    Addition 3. Add line breaks

--- main.py
+++ main.py
@@ -2,6 +2,27 @@ from PIL import Image, ImageDraw, ImageFont
 from os import listdir
 
 
+def split_text(draw, text, font, img_width):
+    lines = []
+    cur_word = 0
+    words = text.split()
+    while cur_word != len(words):
+        # если даже одно слово слишком длинное, скажем об этом пользователю
+        if draw.textsize(words[cur_word], font)[0] > img_width:
+            print("Слово", words[cur_word], "слишком длинное для этой картинки")
+            quit()
+
+        # будем добавлять в текущую строчку слова, пока эта строчка влезает
+        cur_line = []
+        # пока текущая строка с прибавлением нового слова все еще вмещается
+        while cur_word < len(text.split()) and draw.textsize(" ".join(cur_line) + words[cur_word], font)[0] < img_width:
+            # добавляем к текущей строке новое слово
+            cur_line.append(words[cur_word])
+            cur_word += 1
+        lines.append(" ".join(cur_line))
+    return lines
+
+
 def draw_outlined_text(draw, pos, text, font):
     black_color = (0, 0, 0)
     white_color = (255, 255, 255)
@@ -15,9 +36,16 @@ def draw_outlined_text(draw, pos, text, font):
 def draw_text(image, type, text):
     draw = ImageDraw.Draw(image)
     font = ImageFont.truetype("impact.ttf", 50)
-    text_width, text_height = draw.textsize(text, font)  # ширина данного текста с данным шрифтом
-    y_start = 10 if type == "top" else image.height - text_height - 10  # зависит от расположения
-    draw_outlined_text(draw, (image.width / 2 - text_width / 2, y_start), text, font)
+    lines = split_text(draw, text, font, image.width)
+    text_height = draw.textsize(text, font)[1]  # общая высота для каждой строки
+    for i in range(len(lines)):
+        text_width = draw.textsize(lines[i], font)[0]  # ширина данной строки с данным шрифтом
+        if type == "top":
+            y_start = text_height * i + 10
+        else:
+            y_start = image.height - text_height * (len(lines) - i) - 10
+
+        draw_outlined_text(draw, (image.width / 2 - text_width / 2, y_start), lines[i], font)
 
 
 def make_meme(image, top_text="", bottom_text=""):
@@ -48,4 +76,5 @@ def main():
     image.show()
 
 
-main()
+if __name__ == "__main__":
+    main()
